de625049f14beb3bf99fa4c4147f98278ea14505
Carlos Segura Antón <peleino_pilas@hotmail.com>
2019-07-17 14:35:40 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,14 +2,13 @@ window.onload = iniciar;
 
 function iniciar() {
 
-    // Estoy probando con el 9 para ver el error pero el valor correcto es 2
-    var arrayNotasCancion = [9, 2, 2, 2, 2, 2, 2, 4, 0, 1, 2, 3, 3, 3, 3, 2, 2, 2, 1, 1, 2, 1, 4, 2, 2, 2, 2, 2, 2, 2, 4, 0, 1, 2, 3, 3, 3, 3, 2, 2, 4, 4, 3, 1, 0];
+    var numerosNotasCancion = [2, 2, 2, 2, 2, 2, 2, 4, 0, 1, 2, 3, 3, 3, 3, 2, 2, 2, 1, 1, 2, 1, 4, 2, 2, 2, 2, 2, 2, 2, 4, 0, 1, 2, 3, 3, 3, 3, 2, 2, 4, 4, 3, 1, 0];
 
-    var temposNotas = [4, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4];
+    var duracionNotasCancion = [1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4];
 
-    var arraySonidos = $('.sonido');
+    var arraySonidosNotas = $('.sonidoNota');
 
-    $('.nota').click(function () {
+    $('.barraMetal').click(function () {
 
         let numeroNota = parseInt(this.id);
 
@@ -21,11 +20,11 @@ function iniciar() {
 
         if (numeroNota < 9) {
 
-            arraySonidos[numeroNota].pause();
+            arraySonidosNotas[numeroNota].pause();
 
-            arraySonidos[numeroNota].currentTime = 0;
+            arraySonidosNotas[numeroNota].currentTime = 0;
 
-            arraySonidos[numeroNota].play();
+            arraySonidosNotas[numeroNota].play();
 
             $("#" + numeroNota).css("box-shadow", "0px 0px 10px black");
 
@@ -41,13 +40,13 @@ function iniciar() {
 
     $('button').click(function () {
 
-        for (var index = 0; index < arrayNotasCancion.length; index++) {
+        for (var index = 0; index < numerosNotasCancion.length; index++) {
 
             setTimeout(function(y) {
 
-                tocar(arrayNotasCancion[y]);
+                tocar(numerosNotasCancion[y]);
 
-            }, ritmo(index) * 350, index);
+            }, ritmo(index) * 500, index);
 
         }
 
@@ -59,7 +58,7 @@ function iniciar() {
 
         for (index = 0; index < numeroNota; index++) {
 
-            sumaRitmo = sumaRitmo + temposNotas[index];
+            sumaRitmo = sumaRitmo + duracionNotasCancion[index];
 
         }
 
